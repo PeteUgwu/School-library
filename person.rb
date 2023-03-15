0f33e276adc625481ab7a2ee7627a75ece6d38ba
peter <peterugwu64@gmail.com>
2023-03-15 20:42:31 +0100

    Fix linters

--- person.rb
+++ person.rb
@@ -1,22 +1,22 @@
 class Person
-    def initialize(age, name = "Unknown", parent_permission: true)
-        @id = Random.rand(1..1000)
-        @age = age
-        @name = name
-        @parent_permission = parent_permission
-    end
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
+  end
 
-    attr_accessor :age, :name
-    
-    attr_reader :id
+  attr_accessor :age, :name
 
-    def can_use_services?
-        of_age? || @parent_permission
-    end
+  attr_reader :id
 
-    private
+  def can_use_services?
+    of_age? || @parent_permission
+  end
 
-    def of_age? 
-        @age >= 18
-    end
+  private
+
+  def of_age?
+    @age >= 18
+  end
 end
--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,12 @@
 require './person'
 
 class Teacher < Person
-    def initialize(age, specialization, name = "Unknown", parent_permission: true)
-        super(age, name = 'Unknown', parent_permission: true)
-        @specialization = specialization
-    end
+  def initialize(age, specialization, _name = 'Unknown', _parent_permission: true)
+    super(age, _name = 'Unknown', _parent_permission: true)
+    @specialization = specialization
+  end
 
-    def can_use_services?
-        true
-    end
+  def can_use_services?
+    true
+  end
 end
