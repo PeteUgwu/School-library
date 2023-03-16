e7e3b5544e969c78c25383b0c44d88a3d465cee1
peter <peterugwu64@gmail.com>
2023-03-16 19:10:34 +0100

    Prepare base decorator

---
+++ decorator.rb
@@ -0,0 +1,13 @@
+require './nameable'
+
+class Decorator < Nameable
+  
+    def initialize(nameable)
+      @nameable = nameable
+      super()
+    end
+
+    def correct_name
+        @nameable.correct_name
+    end
+end
