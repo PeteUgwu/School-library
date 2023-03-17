ac0ec18583810f2d992a5f5b96c2205a91279c22
peter <peterugwu64@gmail.com>
2023-03-17 16:41:35 +0100

    Fix linters

--- student.rb
+++ student.rb
@@ -1,14 +1,14 @@
 require './person'
 
 class Student < Person
-    attr_reader :classroom
+  attr_reader :classroom
 
   def initialize(age, classroom, _name = 'Unknown', _parent_permission: true)
     super(age, _name = 'Unknown', _parent_permission: true)
     @classroom = classroom
   end
 
-  def classroom(classroom)
+  def classroom=(classroom)
     @classroom = classroom
     classroom.students.push(self) unless classroom.students.include?(self)
   end
