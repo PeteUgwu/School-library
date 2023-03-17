d0e12ef145103dfc0318d14b73b0701bfa7ddacd
peter <peterugwu64@gmail.com>
2023-03-17 16:37:46 +0100

    Update Student, Book and Person classes

--- person.rb
+++ person.rb
@@ -1,8 +1,8 @@
 require './nameable'
 
 class Person < Nameable
-  attr_accessor :age, :name
-  attr_reader :id, :rentals
+  attr_accessor :age, :name, :rentals
+  attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
